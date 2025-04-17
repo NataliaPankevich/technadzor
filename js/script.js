99681b6d01c3813e7d6a415a3d6392d95f8cf733
Nataliya <pankevicnatalia4@gmail.com>
2025-04-17 13:05:16 +0300

    3commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//slider
+
 new Swiper(".projects__slider", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -58,3 +60,57 @@ const phone = document.getElementById("phone");
 var imPhone = new Inputmask("+375 (99) 999-99-99");
 imPhone.mask(phone);
 
+
+
+//modal
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img1 = document.getElementById("myImg1");
+var img2 = document.getElementById("myImg2");
+var img3 = document.getElementById("myImg3");
+var img4 = document.getElementById("myImg4");
+var img5 = document.getElementById("myImg5");
+var img6 = document.getElementById("myImg6");
+
+var modalImg = document.getElementById("img01");
+
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;  
+}
+
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;  
+}
+
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;  
+}
+
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;  
+}
+
+img5.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;  
+}
+
+img6.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;  
+}
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
